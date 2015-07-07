17708cf7cb3e29128b6aae80820f65de3b44e8df
Marcellin Nshimiyimana <mars@fusemachines.com>
2015-07-07 18:14:46 -0400

    use custom addon

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
   name: 'Ember CLI Simple Auth',
 
   included: function(app) {
-    app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth.amd.js', {
+    app.import(app.bowerDirectory + '/ember-simple-auth-input/simple-auth.amd.js', {
       exports: {
         'simple-auth/authenticators/base':                    ['default'],
         'simple-auth/authorizers/base':                       ['default'],
